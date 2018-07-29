de11cd234f6610252661ecfdf6e0e77eeab73bb6
Luis Marin <+LuisMr1997@users.noreply.github.com>
2018-07-29 15:42:09 -0500

    CRUD
    CRUD FUNCIONAL

---
+++ CRUDLuis/Luis/Luis/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Luis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddAddress = new System.Windows.Forms.Button();
+            this.dataGridViewAddress = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textCustomerId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textFullname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridViewCustomer = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAddress)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddAddress
+            // 
+            this.btnAddAddress.Location = new System.Drawing.Point(664, 137);
+            this.btnAddAddress.Name = "btnAddAddress";
+            this.btnAddAddress.Size = new System.Drawing.Size(29, 23);
+            this.btnAddAddress.TabIndex = 4;
+            this.btnAddAddress.Text = "+";
+            this.btnAddAddress.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewAddress
+            // 
+            this.dataGridViewAddress.AllowUserToAddRows = false;
+            this.dataGridViewAddress.AllowUserToDeleteRows = false;
+            this.dataGridViewAddress.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAddress.Location = new System.Drawing.Point(120, 124);
+            this.dataGridViewAddress.Name = "dataGridViewAddress";
+            this.dataGridViewAddress.RowTemplate.Height = 24;
+            this.dataGridViewAddress.Size = new System.Drawing.Size(538, 150);
+            this.dataGridViewAddress.TabIndex = 3;
+            this.dataGridViewAddress.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnAddAddress);
+            this.panel1.Controls.Add(this.dataGridViewAddress);
+            this.panel1.Controls.Add(this.textEmail);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textFullname);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textCustomerId);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(28, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(736, 324);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textCustomerId
+            // 
+            this.textCustomerId.Location = new System.Drawing.Point(120, 23);
+            this.textCustomerId.Name = "textCustomerId";
+            this.textCustomerId.ReadOnly = true;
+            this.textCustomerId.Size = new System.Drawing.Size(100, 22);
+            this.textCustomerId.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Customer ID:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Full name:";
+            // 
+            // textFullname
+            // 
+            this.textFullname.Location = new System.Drawing.Point(120, 54);
+            this.textFullname.Name = "textFullname";
+            this.textFullname.Size = new System.Drawing.Size(238, 22);
+            this.textFullname.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Email:";
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(120, 96);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(238, 22);
+            this.textEmail.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Address:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dataGridViewCustomer
+            // 
+            this.dataGridViewCustomer.AllowUserToAddRows = false;
+            this.dataGridViewCustomer.AllowUserToDeleteRows = false;
+            this.dataGridViewCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCustomer.Location = new System.Drawing.Point(28, 373);
+            this.dataGridViewCustomer.Name = "dataGridViewCustomer";
+            this.dataGridViewCustomer.RowTemplate.Height = 24;
+            this.dataGridViewCustomer.Size = new System.Drawing.Size(736, 150);
+            this.dataGridViewCustomer.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(692, 540);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(72, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(409, 540);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(72, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(505, 540);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(72, 23);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(597, 540);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(72, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // textSearch
+            // 
+            this.textSearch.Location = new System.Drawing.Point(128, 342);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(636, 22);
+            this.textSearch.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 345);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Searchc";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 575);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridViewCustomer);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Luis Marin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAddress)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddAddress;
+        private System.Windows.Forms.DataGridView dataGridViewAddress;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textCustomerId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textFullname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewCustomer;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
